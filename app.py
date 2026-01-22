878685a827a136c7e47e3de57f7b17e8420c55d0
barathbalaji218 <barathbalaji218@gmail.com>
2026-01-22 09:31:53 +0530

    Database URI pass

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from app import create_app
 
@@ -8,4 +9,5 @@ def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
